fd08e57c718366fbb9d207d5fd7982a46deaf752
Carolina Bonini <kriitoboni@gmail.com>
2019-11-03 17:19:20 -0300

    Finish SPRINT 1
    *add filter
    *check sintax error
    *check sort

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-fetch('https://api.imgflip.com/get_memes'),{ method: 'GET'
-})
+fetch('https://api.imgflip.com/get_memes')
   .then(response => {
     if (response.ok){
-        console.log('Successful request!')
+      console.log('Successful request!')
+    } else {
+      console.log('Oops, we got an error ${STATUS_CODE}')
+    }
+    return response.json()
     })
-  .then(response => response.json())
-  .then(memes => {memes.forEach(meme => {const { id, name, width, url } = meme;});})
-  .catch(error => console.log('Oops, we got an error ${STATUS_CODE}'));
+  .then(body => body.data.memes)
+  .then(memes => memes.map(meme => {
+    const { id, name, width, height, url } = meme;
+    return { id, name, width, height, url };
+           }))
+  .then(memesSorted => (memesSorted.filter(meme => meme.width < 500 || meme.height < 500)).sort((a, b) => parseInt(a.id) + parseInt(b.id)))
+  .then(memesSorted => console.log(memesSorted))
+  .catch(err => console.error(err.message));
+
